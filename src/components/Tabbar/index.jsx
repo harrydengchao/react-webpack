a648d0a8aa7c57c1db682d3b4a61b26348a109ea
LAPTOP-4HR5MNDJ\邓超 <dengchao2056@126.com>
2018-12-03 17:38:00 +0800

    让components目录显示

---
+++ src/components/Tabbar/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Index extends Component {
+  render() {
+    return (
+      <ul>
+        <li></li>
+      </ul>
+    )
+  }
+}
+
+export default Index
